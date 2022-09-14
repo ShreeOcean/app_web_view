b2c866ebf92ce52aad6ffe14c55130ec2334a377
Pragyanshree Das <shree.pragyan23@gmail.com>
2022-09-14 19:05:20 +0530

    Done with dependency now pdfview is ready to be code on

--- app/src/main/java/com/ocean/webviewimagepdf/Constant.java
+++ app/src/main/java/com/ocean/webviewimagepdf/Constant.java
@@ -4,9 +4,11 @@ public class Constant {
 
     public static final String API_URL = "http://dev-test-erp.co.in/basic/api_tracker/";
 
+    public static final String auth_key = "f0ecd33f8ddbbee98522ca726745c5e4";
+    public static final String id = "3602";
+
     public static final String USER_LOGIN = API_URL+"login_check.php";
     public static final String ALL_JOURNALLISTBYME= API_URL+"all_journallistbyme.php";
     public static final String VIEW_JOURNAL_EXPENSES = API_URL + "view_journal_expense.php";
-
 
 }
